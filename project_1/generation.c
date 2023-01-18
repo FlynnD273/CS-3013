0d7055ad6162378ab73546debc128b1169cb0318
Flynn Duniho <flynn@harveyosity.org>
2023-01-18 13:09:02 -0500

    finished makefile, should work for all other problems too

--- project_1/generation.c
+++ project_1/generation.c
@@ -6,21 +6,34 @@
 #include "seed_reader.h"
 
 int main () {
+	// Initialize the seed from the seed.txt file
 	int seed = readseed();
 	srand(seed);
-	int childCount = (rand() % 8) + 5;
-	printf("seed: %i\ncount: %i\n", seed, childCount);
 
-	int status;
+	// Set the number of children to make, from the seeded value
+	int childcount = (rand() % 8) + 5;
 
-	while (childCount > 0) {
-		childCount--;
-		pid_t child = fork();
-		if (child == 0) {
-			waitpid(child, &status, WUNTRACED);
+	while (childcount > 0) {
+		pid_t childpid = fork();
+
+		// Parent
+		if (childpid != 0) {
+			printf("[Parent, PID: %i]: I am waiting for PID %i to finish\n", getpid(), childpid);
+
+			int status;
+			waitpid(childpid, &status, WUNTRACED);
+
+			printf("[Parent, PID: %i]: Child %i finished with status code %i. It's now my turn to exit.\n"
+			, getpid(), childpid, WEXITSTATUS(status));
 			break;
 		}
+		// Child
+		else {
+			printf("\t[Child, PID: %i]: I was called with descendant count=%i. I\'ll have %i descendants(s).\n"
+			, getpid(), childcount, childcount - 1);
+		}
+		childcount--;
 	}
-	printf("childCount: %i\n", childCount);
-	return childCount;
+
+	return childcount;
 }
