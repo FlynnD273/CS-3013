3810e2f45f36864ac185ef252ff4e95b87040aae
Flynn Duniho <flynn@harveyosity.org>
2023-01-22 17:33:13 -0500

    finish slugrace

--- project_1/slugrace.c
+++ project_1/slugrace.c
@@ -39,21 +39,45 @@ int main() {
 	
 	struct timespec start;
 	clock_gettime(CLOCK_REALTIME, &start);
+	
+	struct timespec lastprint;
+	clock_gettime(CLOCK_REALTIME, &lastprint);
 
 	int racingchildren = 4;
 	while (racingchildren > 0) {
 		int st = waitpid(-1, NULL, WNOHANG);
+		struct timespec end;
+		clock_gettime(CLOCK_REALTIME, &end);
+
 		if (st <= 0) {
 			// check for last print time, if it's larger than 0.33 seconds
 
-			// print all running children
+			float lastprintdelta = end.tv_sec - lastprint.tv_sec + (end.tv_nsec - lastprint.tv_nsec) / 1000000000.0;
+			if (lastprintdelta >= 0.33) {
+				clock_gettime(CLOCK_REALTIME, &lastprint);
+
+				// print all running children
+				printf("The race is ongoing. The following children are still racing: ");
+				for (int i = 0; i < numchildren; i++) {
+					if (kids[i] != 0) {
+						printf("%d ", kids[i]);
+					}
+				}
+				printf("\n");
+			}
+
+			continue;
 		}
 
 		--racingchildren;
 
-		struct timespec end;
-		clock_gettime(CLOCK_REALTIME, &end);
-		float delta = end.tv_sec - start.tv_sec + (end.tv_nsec - end.tv_nsec) / 1000000000.0;
+		for (int i = 0; i < numchildren; i++) {
+			if (kids[i] == st) {
+				kids[i] = 0;
+			}
+		}
+
+		float delta = end.tv_sec - start.tv_sec + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
 
 		printf("Child %d has crossed the finish line! It took %f seconds\n", st, delta);
 	}
