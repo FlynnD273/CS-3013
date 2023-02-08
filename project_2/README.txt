58a30d7faff40f18cc0e84fb02177976c929f558
Bernhardt Adler <lbadler@wpi.edu>
2023-02-07 19:03:19 -0500

    small changes, added a lot to readme

--- project_2/README.txt
+++ project_2/README.txt
@@ -1,5 +1,11 @@
-We first started out by reading through the two problems and figuring out which one we wanted to do. We decided on teh second problem because it seemed like the more definied of the two, and it looked a little more interesting to do. 
+We first started out by reading through the two problems and figuring out which one we wanted to do. We decided on teh second problem because it seemed like the more definied of the two, and it looked a little more interesting to do.
 
-The next step was to make sure that we understood the thread creation logic. So we added just eh bare minimum needed to spawn all the plane threads and print out the plane number. 
+The next step was to make sure that we understood the thread creation logic. So we added just the bare minimum needed to spawn all the plane threads and print out the plane number.
 
-After that, we added skeleton methods to establish how the planes would loop through and be controlled. Once completing that, we moved on to the actual threading logic. 
+After that, we added skeleton methods to establish how the planes would loop through and be controlled. Once completing that, we moved on to the actual threading logic.
+
+Initially, we used ints to indicate which segments of runway were claimed by which plane, with -1 for unclaimed. Eventually, we decided to replace this with a system of semaphores rather than ints.
+
+Once the semaphores were in place, we began implementing the action loop for the small planes. The first method we implemented was await_runway, which decided on which path the plane would take and then waited for all segments to be available. We did this by having the plane sem_wait() on the first segment of its path, and when that segment became ready, it would check if its other segment is also available. If it is, the thread would claim both semaphores and the method would return the path. if not, it would release its hold on the initial semaphore and return to the back of the queue.
+
+Once this was in place, the remaining methods for each plane were relatively much easier, and once we finished implementing our methods for the small planes we simply made slightly modified verisons for the large planes.
--- project_2/flight.c
+++ project_2/flight.c
@@ -25,7 +25,7 @@ int get_small_plane_number(int i) {
 
 int await_runway_large(int i) {
 	int sequence = rand() % 4;
-	printf("Plane #%d (Large) is waiting for runway regions %i, %i, and %i\n", i, longpaths[sequence][0], longpaths[sequence][1], longpaths[sequence][2]);
+	printf("Plane #%d (Large) is waiting for runway regions %i, %i, and %i\n", i, longpaths[sequence][0]+1, longpaths[sequence][1]+1, longpaths[sequence][2]+1);
 	bool clear = false;
 	while (!clear) {
 		sem_wait(&regions[longpaths[sequence][0]]);
@@ -61,10 +61,10 @@ int await_takeoff_large(int i) {
 
 void takeoff_large(int i, int path_index) {
 	int delay;
-	printf("Plane #%d (Large) is taking off on runway regions %i, %i, and %i\n", i, longpaths[path_index][0], longpaths[path_index][1], longpaths[path_index][2]);
+	printf("Plane #%d (Large) is taking off on runway regions %i, %i, and %i\n", i, longpaths[path_index][0]+1, longpaths[path_index][1]+1, longpaths[path_index][2]+1);
 	for (int j = 0; j < 3; j++) {
 		delay = (rand() % 4) + 1;
-		printf("Plane #%i (Large) has entered region %i while taking off\nWaiting %i seconds\n", i, longpaths[path_index][j], delay);
+		printf("Plane #%i (Large) has entered region %i while taking off\nWaiting %i seconds\n", i, longpaths[path_index][j]+1, delay);
 		sleep(delay);
 		sem_post(&regions[longpaths[path_index][j]]);
 	}
@@ -85,10 +85,10 @@ int await_land_large(int i) {
 
 void land_large(int i, int path_index) {
 	int delay;
-	printf("Plane #%d (Large) is landing on runway regions %i, %i, and %i\n", i, longpaths[path_index][0], longpaths[path_index][1], longpaths[path_index][2]);
+	printf("Plane #%d (Large) is landing on runway regions %i, %i, and %i\n", i, longpaths[path_index][0]+1, longpaths[path_index][1]+1, longpaths[path_index][2]+1);
 	for (int j = 0; j < 3; j++) {
 		delay = (rand() % 4) + 1;
-		printf("Plane #%i (Large) has entered region %i while landing\nWaiting for %i seconds\n", i, longpaths[path_index][j], delay);
+		printf("Plane #%i (Large) has entered region %i while landing\nWaiting for %i seconds\n", i, longpaths[path_index][j]+1, delay);
 		sleep(delay);
 		sem_post(&regions[longpaths[path_index][j]]);
 	}
@@ -116,7 +116,7 @@ int spawn_large(int i) {
 
 int await_runway_small(int i) {
 	int sequence = rand() % 14;
-	printf("Plane #%d (Small) is waiting for runway regions %i and %i\n", get_small_plane_number(i), shortpaths[sequence][0], shortpaths[sequence][1]);
+	printf("Plane #%d (Small) is waiting for runway regions %i and %i\n", get_small_plane_number(i), shortpaths[sequence][0]+1, shortpaths[sequence][1]+1);
 	bool clear = false;
 	while (!clear) {
 		sem_wait(&regions[shortpaths[sequence][0]]);
@@ -144,10 +144,10 @@ int await_takeoff_small(int i) {
 
 void takeoff_small(int i, int path_index) {
 	int delay;
-	printf("Plane #%d (Small) is taking off on runway regions %i and %i\n", get_small_plane_number(i), shortpaths[path_index][0], shortpaths[path_index][1]);
+	printf("Plane #%d (Small) is taking off on runway regions %i and %i\n", get_small_plane_number(i), shortpaths[path_index][0]+1, shortpaths[path_index][1]+1);
 	for (int j = 0; j < 2; j++) {
 		delay = (rand() % 4) + 1;
-		printf("Plane #%i (Small) has entered region %i while taking off\nWaiting %i seconds\n", get_small_plane_number(i), shortpaths[path_index][j], delay);
+		printf("Plane #%i (Small) has entered region %i while taking off\nWaiting %i seconds\n", get_small_plane_number(i), shortpaths[path_index][j]+1, delay);
 		sleep(delay);
 		sem_post(&regions[shortpaths[path_index][j]]);
 	}
@@ -168,10 +168,10 @@ int await_land_small(int i) {
 
 void land_small(int i, int path_index) {
 	int delay;
-	printf("Plane #%d (Small) is landing on runway regions %i and %i\n", get_small_plane_number(i), shortpaths[path_index][0], shortpaths[path_index][1]);
+	printf("Plane #%d (Small) is landing on runway regions %i and %i\n", get_small_plane_number(i), shortpaths[path_index][0]+1, shortpaths[path_index][1]+1);
 	for (int j = 0; j < 2; j++) {
 		delay = (rand() % 4) + 1;
-		printf("Plane #%i (Small) has entered region %i while landing\nWaiting for %i seconds\n", get_small_plane_number(i), shortpaths[path_index][j], delay);
+		printf("Plane #%i (Small) has entered region %i while landing\nWaiting for %i seconds\n", get_small_plane_number(i), shortpaths[path_index][j]+1, delay);
 		sleep(delay);
 		sem_post(&regions[shortpaths[path_index][j]]);
 	}
